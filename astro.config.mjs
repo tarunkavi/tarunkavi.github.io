0a71cd7ad15917e4a0d97767eb6e1b4df209ea52
tarun-kavipurapu <tarunsharmakavipurapu@gmail.com>
2025-12-07 22:45:48 +0530

    changes made

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://tarunkavi.github.io/', // Replace with your site URL
+    base: '/tarunkavi', // Replace with your repo name
+});
